c65d66ec43ab920293f5a741bef7ea663c2ccdc5
gabrielkerekes <gabriel.kerekes@vacuumlabs.com>
2025-03-05 09:53:48 +0100

    Fix local build

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,19 @@
 import type { NextConfig } from "next";
 
+const isProduction = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
   /* config options here */
-  output: "export",
-  // Ensure the base path matches your GitHub repository name
-  // For example, if your repo is username.github.io/repo-name, use '/repo-name'
-  basePath: "/evm-dapp-connector-playground",
-  // This is needed for GitHub Pages
-  images: {
-    unoptimized: true,
-  },
+  ...(isProduction && {
+    output: "export",
+    // Ensure the base path matches your GitHub repository name
+    // For example, if your repo is username.github.io/repo-name, use '/repo-name'
+    basePath: "/evm-dapp-connector-playground",
+    // This is needed for GitHub Pages
+    images: {
+      unoptimized: true,
+    },
+  }),
 };
 
 export default nextConfig;
